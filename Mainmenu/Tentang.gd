27f296f7268c13a34ee1481914d2e3f10abaaf29
DreamNinjas <mziyadrgt@gmail.com>
2023-12-11 21:18:40 +0800

    add transition

--- Mainmenu/Tentang.gd
+++ Mainmenu/Tentang.gd
@@ -1,10 +1,8 @@
 extends Control
 
 
-
-
-func _on_kembali_button_up():
+func _on_button_button_up():
 	var transition_node = get_node("/root/Control/Transition")
 	transition_node.play()
 	await get_tree().create_timer(0.5).timeout
-	visible = false;
+	$Tentang.visible = true;
---
+++ global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@onready var music = true;
+@onready var sfx = true;

d2c61ec58f30a3e2065d92079aa67b4cc1633d7e
DreamNinjas <mziyadrgt@gmail.com>
2023-12-13 15:27:38 +0800

    added fake loading screen

--- Mainmenu/mainmenu.gd
+++ Mainmenu/mainmenu.gd
@@ -4,6 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Transition.play()
+	$Move.play("moving")
 	await get_tree().create_timer(0.5).timeout
 	$BGM.play()
 
